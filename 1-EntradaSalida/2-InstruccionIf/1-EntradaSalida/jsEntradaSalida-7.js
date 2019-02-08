cf1fadd5836bb89e43953562fa808ae92246f88e
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2019-02-08 12:28:53 -0300

    segunda clase

---
+++ 1-EntradaSalida/2-InstruccionIf/1-EntradaSalida/jsEntradaSalida-7.js
@@ -0,0 +1,61 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+    var numerouno;
+    var numerodos;
+    var resultado;
+
+    numerouno = document.getElementById("numeroUno").value;
+    numerodos = document.getElementById("numeroDos").value;
+numerouno= parseInt(numerouno);
+numerodos= parseInt(numerodos);
+
+resultado= numerouno + numerodos;
+alert("la suma es" + resultado);
+} 
+
+function restar()
+{
+    var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = document.getElementById("numeroUno").value;
+    numerodos = document.getElementById("numeroDos").value;
+numerouno= parseInt(numerouno);
+numerodos= parseInt(numerodos);
+
+resultado= numerouno - numerodos;
+alert ("la resta es" + resultado);
+}
+
+function multiplicar()
+{ 
+    var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = document.getElementById("numeroUno").value;
+    numerodos = document.getElementById("numeroDos").value;
+numerouno= parseInt(numerouno);
+numerodos= parseInt(numerodos);
+
+resultado= numerouno * numerodos;
+alert("la multiplicacion es" + resultado);
+}
+
+function dividir()
+{
+	var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = document.getElementById("numeroUno").value;
+    numerodos = document.getElementById("numeroDos").value;
+numerouno= parseInt(numerouno);
+numerodos= parseInt(numerodos);
+
+resultado= numerouno / numerodos;
+alert("la division es" + resultado);
+}
+
