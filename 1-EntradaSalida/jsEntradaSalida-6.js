aad56da90c3bac7ee4c5309bfdc648740b415cba
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2019-02-10 16:50:37 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,15 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+var numerouno;
+var numerodos;
+var resultado;
+numerouno=document.getElementById("numeroUno").value;
+numerodos=document.getElementById("numeroDos").value;
+numerouno=parseInt(numerouno);
+numerodos=parseInt(numerodos);
+
+resultado =numerouno+numerodos;
+alert ("el resultado es" + resultado );
 }
 
