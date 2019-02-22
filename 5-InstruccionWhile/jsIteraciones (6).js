57b5912e755b630e0948336087fd7542ac1def2c
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2019-02-22 12:29:08 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,13 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 0;
+	var acumulador = 0;
+	var numero;
+	var promedio;
 
+while (contador < 5 )
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / 5;
 
 }//FIN DE LA FUNCIÓN
